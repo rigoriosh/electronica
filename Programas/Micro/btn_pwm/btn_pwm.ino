5d11892590928cd155dc6ba1ff4a4ccb8eb85460
rigoriosh <rigoriosh@gmail.com>
2021-10-20 18:33:00 -0500

    201020211832

---
+++ Programas/Micro/btn_pwm/btn_pwm.ino
@@ -0,0 +1,20 @@
+const int btn_TRIGGER = 4;
+const int salida_pwm_TEMP = 5;
+void setup() {
+
+  pinMode(btn_TRIGGER, INPUT_PULLUP);
+  pinMode(salida_pwm_TEMP, OUTPUT);
+
+  
+}
+
+void loop() {
+  
+  if(digitalRead(btn_TRIGGER)==LOW){
+    analogWrite(salida_pwm_TEMP, 0);
+  }else{
+    analogWrite(salida_pwm_TEMP, 255);
+  }
+
+
+}
--- Programas/Micro/potenciometer/potenciometer.ino
+++ Programas/Micro/potenciometer/potenciometer.ino
@@ -1,7 +1,10 @@
 int testpotenciometer = A1;
 float lecturaPotenciometro = 0;
 float voltajeOut = 0;
-
+int memoryLectura = 0;
+long sumaMuestra = 0;
+int promedio = 0;
+int muestras = 5;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -9,24 +12,51 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  lecturaPotenciometro = map(analogRead(testpotenciometer), 0, 1023, 0, 10);
-  //float volSalida = ( 255 * lecturaPotenciometro)/ 1023;
-  // analogWrite(13, lecturaPotenciometro);
-  Serial.print("lecturaPotenciometro => ");
-  Serial.println(lecturaPotenciometro);
+  // toma muestras y realiza promedio
 
-  if(lecturaPotenciometro == 0){
-    digitalWrite(LED_BUILTIN, LOW);
-  }else{
-    for(int i = 0; i < 500; i++){
-      digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-      delayMicroseconds(lecturaPotenciometro);                       // wait for a second
-      digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-      delayMicroseconds(lecturaPotenciometro);
-    }  
+  
+  sumaMuestra = 0;
+  promedio = 0;
+  for(int muestra = 0 ; muestra <= 10; muestra ++){
+    lecturaPotenciometro = analogRead(testpotenciometer);
+    sumaMuestra = sumaMuestra + lecturaPotenciometro;
   }
+  promedio = sumaMuestra / 10;
 
+if(promedio < 10){
+  promedio = 1;
+}else if (promedio > 1000){
+  promedio = 1023;
+}
+
+if(promedio != memoryLectura){
+  memoryLectura = promedio;
+  // float volSalida = ( 255 * lecturaPotenciometro)/ 1023;
+  // analogWrite(13, lecturaPotenciometro);
+  Serial.println("///***************\\\\");
+  int mapeado = map(promedio, 1, 1023, 0, 255);
+  Serial.print("promedio => ");
+  Serial.println(promedio);
+  Serial.print("mapeado => ");
+  Serial.println(mapeado);
+
+  analogWrite(3, mapeado);
+  
+//  if(mapeado <= 3){
+//    //digitalWrite(LED_BUILTIN, LOW);
+//    analogWrite(3, 0);
+//  }else{
+//    
+//    for(int i = 0; i < 500; i++){
+//      digitalWrite(LED_BUILTIN, HIGH);   
+//      delayMicroseconds(lecturaPotenciometro);                      
+//      digitalWrite(LED_BUILTIN, LOW);   
+//      delayMicroseconds(lecturaPotenciometro);
+//    }  
+//  }
+
+}
+  
   
   
 }
